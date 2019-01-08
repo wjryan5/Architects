93669e0a2a1cc8bc6c08694888c7b7164491816e
wjryan5 <43756110+wjryan5@users.noreply.github.com>
2019-01-08 18:57:00 +0000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-
+/* Local Storage */
     function WebName()
     {
       if (typeof(Storage) !== "undefined") {
          if (localStorage.getItem("lastname")===null)
          {
-            alert("Localstorage value not yet set");
             var name = prompt("Please enter your name");
             localStorage.setItem("lastname", name);
          }
@@ -21,3 +20,68 @@
       }
    }
 
+   /* This jQuery is for the modal boxes */
+   $(document).ready(function () 
+   {
+      console.log("ready!");
+  
+      $(".myBtnED").click(function () 
+      {
+          $(".overlayED").show();
+          setTimeout(function () { $(".overlayED").hide(); }, 15000);
+      });
+  
+      $(".close").click(function () 
+      {
+          $(".overlayED").hide();
+      });
+  
+  });
+  $(document).ready(function () 
+  {
+   console.log("ready!");
+
+   $(".myBtnAL").click(function () 
+   {
+       $(".overlayAL").show();
+       setTimeout(function () { $(".overlayAL").hide(); }, 15000);
+   });
+
+   $(".close").click(function () 
+   {
+       $(".overlayAL").hide();
+   });
+
+});
+$(document).ready(function () 
+{
+   console.log("ready!");
+
+   $(".myBtnAW").click(function () 
+   {
+       $(".overlayAW").show();
+       setTimeout(function () { $(".overlayAW").hide(); }, 15000);
+   });
+
+   $(".close").click(function () 
+   {
+       $(".overlayAW").hide();
+   });
+
+});
+$(document).ready(function () 
+{
+   console.log("ready!");
+
+   $(".myBtnTS").click(function () 
+   {
+       $(".overlayTS").show();
+       setTimeout(function () { $(".overlayTS").hide(); }, 15000);
+   });
+
+   $(".close").click(function () 
+   {
+       $(".overlayTS").hide();
+   });
+
+});
